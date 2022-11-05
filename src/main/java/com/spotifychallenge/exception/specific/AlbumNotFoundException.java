11a7690f5e0dbb6e558a3624991ac0cc4e755eeb
Maël DONNART <mael.donnart1@gmail.com>
2022-11-05 21:57:29 +0100

    fix: Parse release date to LocalDate

--- src/main/java/com/spotifychallenge/exception/specific/AlbumNotFoundException.java
+++ src/main/java/com/spotifychallenge/exception/specific/AlbumNotFoundException.java
@@ -3,4 +3,8 @@ package com.spotifychallenge.exception.specific;
 import com.spotifychallenge.exception.SpotifyChallengeException;
 
 public class AlbumNotFoundException extends SpotifyChallengeException {
+
+    public AlbumNotFoundException(String message) {
+        super(message);
+    }
 }
