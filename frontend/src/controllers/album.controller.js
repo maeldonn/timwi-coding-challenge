d82b295bbc5a37743b7f6890b9d4bd0b9103b93b
Maël DONNART <mael.donnart1@gmail.com>
2022-11-06 16:13:02 +0100

    refactor: Handle error api front side

--- frontend/src/controllers/album.controller.js
+++ frontend/src/controllers/album.controller.js
@@ -1,36 +1,56 @@
 export async function getAlbums(filter) {
-    const reponse = await fetch(`${process.env.VUE_APP_ROOT_API}/albums?searchFilter=${filter}`, {
-        method: 'GET'
-    });
-    return reponse.json();
+    try {
+        const reponse = await fetch(`${process.env.VUE_APP_ROOT_API}/albums?searchFilter=${filter}`, {
+            method: 'GET'
+        });
+        return reponse.json();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function getPersonalAlbums() {
-    const reponse = await fetch(`${process.env.VUE_APP_ROOT_API}/albums/personal`, {
-        method: 'GET'
-    });
-    return reponse.json();
+    try {
+        const reponse = await fetch(`${process.env.VUE_APP_ROOT_API}/albums/personal`, {
+            method: 'GET'
+        });
+        return reponse.json();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function addAlbumToPersonalList(id, callback) {
-    await fetch(`${process.env.VUE_APP_ROOT_API}/albums/${id}`, {
-        method: 'POST'
-    });
-    callback();
+    try {
+        await fetch(`${process.env.VUE_APP_ROOT_API}/albums/${id}`, {
+            method: 'POST'
+        });
+        callback();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function removeAlbumFromPersonalList(id, callback) {
-    await fetch(`${process.env.VUE_APP_ROOT_API}/albums/${id}`, {
-        method: 'DELETE'
-    });
-    callback();
+    try {
+        await fetch(`${process.env.VUE_APP_ROOT_API}/albums/${id}`, {
+            method: 'DELETE'
+        });
+        callback();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export async function toggleFavoriteAlbum(album, callback) {
-    await fetch(`${process.env.VUE_APP_ROOT_API}/albums`, {
-        method: 'PUT',
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify(album)
-    });
-    callback();
+    try {
+        await fetch(`${process.env.VUE_APP_ROOT_API}/albums`, {
+            method: 'PUT',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(album)
+        });
+        callback();
+    } catch (error) {
+        console.error(error);
+    }
 }
--- frontend/src/controllers/tag.controller.js
+++ frontend/src/controllers/tag.controller.js
@@ -1,8 +1,12 @@
 export async function createTag(tag, callback) {
-    await fetch(`${process.env.VUE_APP_ROOT_API}/tags`, {
-        method: 'POST',
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify(tag)
-    });
-    callback();
+    try {
+        await fetch(`${process.env.VUE_APP_ROOT_API}/tags`, {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(tag)
+        });
+        callback();
+    } catch (error) {
+        console.error(error);
+    }
 }
