65e7d9278756d6b247d4dbf62e3347df1f23149c
Maël DONNART <mael.donnart1@gmail.com>
2022-11-06 17:30:50 +0100

    refactor: Entity sequence generator

--- src/main/java/com/spotifychallenge/entity/TagEntity.java
+++ src/main/java/com/spotifychallenge/entity/TagEntity.java
@@ -6,8 +6,10 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +21,8 @@ import java.util.List;
 public class TagEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TAG_GENERATOR")
+    @SequenceGenerator(name = "TAG_GENERATOR", sequenceName = "SEQ_TAG_GENERATOR", allocationSize = 1)
     @Column(name = "ID")
     private Long id = null;
 
